183b59df5ab3f6a7f92d9e7334820839895611f5
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-01-24 20:42:35 +0200

    solve categorie feature

--- models/__tests__/categories.test.js
+++ models/__tests__/categories.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Categories = require('../categories/categories.js');
 
 describe('Categories Model', () => {
@@ -31,5 +33,35 @@ describe('Categories Model', () => {
           });
       });
   });
-
+  it('can update() a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        record.name = 'Test for update category'
+            return categories.update(record._id,record)
+            .then(category => {
+            return categories.get(category._id)
+            .then(category=>{
+              console.log(category)
+              Object.keys(obj).forEach(key => {
+                expect(category[0][key]).toEqual(obj[key]);
+              });
+            })
+          });
+      });
+  });
+  it('can delete() a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        return categories.get(record._id)
+          .then(category => {
+            return categories.delete(category._id)
+            .then(category=>{
+              console.log(category)
+              expect(category).toEqual();
+            })
+          });
+      });
+  });
 });
